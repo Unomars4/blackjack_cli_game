b36f08908084f7e8721f37361522ad8a867a9a59
Unomars4 <une.nyide@gmail.com>
2023-04-26 14:01:19 +0200

    add getBet and getDeck function

--- blackjack.py
+++ blackjack.py
@@ -21,23 +21,53 @@ but must hit exactly one more time before standing.
 In case of a tie, the bet is returned to the player.
 The dealer stops hitting at 17.""")
 
-money = 5000
-while True:
+    money = 5000
+    while True:
 
-    if money <= 0:
-        print("You're out of funds")
-        print("Good thing, this wasn't real money.")
-        print("Thanks for playing!")
-        sys.exit()
+        if money <= 0:
+            print("You're out of funds")
+            print("Good thing, this wasn't real money.")
+            print("Thanks for playing!")
+            sys.exit()
 
-    print(f"MONEY: ${money}")
-    bet = getBet(money)
+        print(f"Money: ${money}")
+        bet = getBet(money)
+        if not bet:
+            break
+
+        deck = getDeck()
+        dealerhand = [deck.pop(),deck.pop()]
+        playerhand = [deck.pop(),deck.pop()]
     
-    return 0
+    
+    print("Thanks for playing")
+
 
 def getBet(maxBet):
-    print("How much ")
+    
+    bet = 0
+    while True:
+        print(f"How much do you want to bet this round? (${maxBet} or QUIT)")
+        response = input("> ").upper()
+        if response == "QUIT":
+            break
+        if not response.isdecimal():
+            continue
+        if 0 < int(response) <= maxBet:
+            bet = int(response)
+            break
+    return bet
 
+def getDeck():
+    
+    deck = []
+    for suit in [DIAMONDS, HEARTS, SPADES, CLUBS]:
+        for rank in range(2,11):
+            deck.append((rank, suit))
+        for rank in ["J", "K", "Q", "A"]:
+            deck.append((rank, suit))
+    random.shuffle(deck)
+    return deck
 
 
 if __name__ == "__main__":
